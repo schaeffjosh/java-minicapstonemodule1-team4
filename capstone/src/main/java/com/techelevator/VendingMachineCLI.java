8d9725987ed89503eed721d4b8bbb7b5be22748a
Tori Brenneison <tori.brenneison@gmail.com>
2023-10-05 08:56:45 -0400

    fix repo

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,34 +1,19 @@
 package com.techelevator;
 
-import com.techelevator.view.Menu;
 
 public class VendingMachineCLI {
 
-	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
-	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
 
-	private Menu menu;
+	public VendingMachineCLI() {
 
-	public VendingMachineCLI(Menu menu) {
-		this.menu = menu;
 	}
 
 	public void run() {
-		while (true) {
-			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
-
-			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
-			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				// do purchase
-			}
-		}
+		System.out.println("Hello world!");
 	}
 
 	public static void main(String[] args) {
-		Menu menu = new Menu(System.in, System.out);
-		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		VendingMachineCLI cli = new VendingMachineCLI();
 		cli.run();
 	}
 }
